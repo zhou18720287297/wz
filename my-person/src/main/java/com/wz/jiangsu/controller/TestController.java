ae3c6ba5a6f9de902ee01308998d4acc15b7f76a
zhou18720287297 <wangzhou_2619@163.com>
2023-11-27 21:42:59 +0800

    init 2

--- my-person/src/main/java/com/wz/jiangsu/controller/TestController.java
+++ my-person/src/main/java/com/wz/jiangsu/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.wz.jiangsu.controller;
 
 import cn.hutool.core.util.StrUtil;
+import com.wz.jiangsu.bean.Event;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
