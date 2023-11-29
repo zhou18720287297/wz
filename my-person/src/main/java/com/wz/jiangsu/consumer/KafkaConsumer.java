b03e69bfa88778eb4582c5441420883afff9be6d
wz062619 <wangzhou_2619@163.com>
2023-11-29 17:15:45 +0800

    测试：切换环境到 dev 环境

--- my-person/src/main/java/com/wz/jiangsu/consumer/KafkaConsumer.java
+++ my-person/src/main/java/com/wz/jiangsu/consumer/KafkaConsumer.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 /**
  * @program: wz
  * @author: wangzhou
@@ -17,50 +15,31 @@ import java.util.List;
 public class KafkaConsumer {
 
     @Value("${kafka.topic}")
-    private  String testTopic;
+    private String testTopic;
+
     // 消费监听
     @KafkaListener(topics = {"analysisd_alert_log"})
-    public void onMessage1(List<ConsumerRecord<?, ?>> records){
-        System.out.println("=================");
-
-        String str = (String)records.get(0).value();
-        System.out.println(str);
-
-
-
-        // 消费的哪个topic、partition的消息,打印出消息内容
-//        records.forEach(record -> {
-//            System.out.println("简单消费Topic："+record.topic()+"**分区"+record.partition()+"**值内容"+record.value());
-//        });
+    public void onMessage1(ConsumerRecord<?, ?> record) {
+        System.out.println("+++++++++++++++++");
+        String value = (String) record.value();
+        System.out.println(value);
 
     }
 
     @KafkaListener(topics = {"analysisd_alert_json"})
-    public void onMessage2(List<ConsumerRecord<?, ?>> records){
-        System.out.println("++++++++++++");
-        String value = (String) records.get(0).value();
+    public void onMessage2(ConsumerRecord<?, ?> record) {
+        System.out.println("=============");
+        String value = (String) record.value();
         System.out.println(value);
-
-//        records.forEach(record -> {
-//            System.out.println("简单消费Topic："+record.topic()+"**分区"+record.partition()+"**值内容"+record.value());
-//        });
     }
 
 
     @KafkaListener(topics = {"testTopic"})
-    public void onMessage3(List<ConsumerRecord<String, String>> record){
-        System.out.println(record);
-        // 消费的哪个topic、partition的消息,打印出消息内容
-        record.forEach(n -> System.out.println(n.value()));
-//        System.out.println("简单消费Topic："+record.topic()+"**分区"+record.partition()+"**值内容"+record.value());
+    public void onMessage3(ConsumerRecord<?, ?> record) {
+        System.out.println(record + "：102机器的 Kafka");
+        System.out.println("=============");
+        String value = (String) record.value();
+        System.out.println(value);
+
     }
-
-    //    @KafkaListener(topics = {"aaa"})
-//    public void onMessage3(ConsumerRecord<?, ?> record){
-//        // 消费的哪个topic、partition的消息,打印出消息内容
-//        System.out.println("简单消费Topic："+record.topic()+"**分区"+record.partition()+"**值内容"+record.value());
-//    }
-
-
-
 }
